5534efcbfacef0bb36ab80adec7238f77ef0d098
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-10-11 20:34:38 -0300

    modifying express.static

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-// const http = require('http');
-// const fs = require('fs');
-// const about =  fs.readFileSync('about.html');
-// const home = fs.readFileSync('index.html');
-
-// const server = http.createServer((req,res)=>{
-//   if(req.url === '/about') {
-//      res.writeHead(200)
-//     res.end(about)
-//   }else {
-//    res.end(home)
-//   }
-// })
-
-// server.listen(3100,()=>{
-//  console.log('Server Running')
-// });
-
 
 const express = require('express');
 const app = express();
-const path = require('path');
 app.use(express.static('public'));
+const path = require('path');
 const port = 300;
 
 app.listen(port,()=>{
